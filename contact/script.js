d4a4001eecae7f53560119c5bc825e031d55b67e
sakai-kiyoshi <77627513+sakai-kiyoshi@users.noreply.github.com>
2021-02-25 17:44:06 +0900

    Add files via upload

---
+++ contact/script.js
@@ -0,0 +1,22 @@
+$(function() {
+
+    $('.slider').slick({
+        autoplay: true,
+        dots: true,
+    });
+});
+
+$(document).ready(function() {
+  var pagetop = $('.pagetop');
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+       } else {
+            pagetop.fadeOut();
+            }
+       });
+       pagetop.click(function () {
+           $('body, html').animate({ scrollTop: 0 }, 500);
+              return false;
+   });
+});
